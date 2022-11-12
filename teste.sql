3da4b55949b6a6b74355e2a15311a38e1636965e
Leonardo Jordão de Cezare <Lcezare@gmail.com>
2022-11-12 17:21:47 -0300

    12/11/2022

--- teste.sql
+++ teste.sql
@@ -1,3 +1,10 @@
+# Atualizar registro
+UPDATE usuarios SET nome = "Nome de Teste" WHERE nome = "Luis Silva";
+
+# Deletar registro
+DELETE FROM usuarios WHERE nome = "Victor";
+
+# Criar tabelas
 CREATE TABLE usuarios (
     nome VARCHAR(50),
     email VARCHAR(100),
@@ -9,3 +16,6 @@ INSERT INTO usuarios(nome, email, idade) VALUES(
     "Lcezare@gmail.com",
     33    
 );
+
+# Acessar banco de dados pelo terminal
+mysql -u root -p usuarios < database_name. sql
